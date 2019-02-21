9439b9572f824faad7c68cbf4d0e38f8d2cee8f0
daniel_rodriguez <daniel.rodriguez@sophossolutions.com>
2019-02-21 12:35:40 -0500

    agregando extensión al archivo

--- lib/pipelines/testapp.rb
+++ lib/pipelines/testapp.rb
@@ -5,7 +5,7 @@ require 'selenium-webdriver'
 class TestApp
 
     def ProbarGoogle
-        Selenium::WebDriver::Chrome.driver_path=ENV['ChromeWebDriver']
+        Selenium::WebDriver::Chrome.driver_path=ENV['ChromeWebDriver'] + ".exe"
         browser=Selenium::WebDriver.for :chrome
         browser.navigate.to "http://www.google.com"
         #puts "Google Loaded in Chrome Browser"
