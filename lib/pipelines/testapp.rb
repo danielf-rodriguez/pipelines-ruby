09d339bfcc7ffa498cdc7dfdab316e2bf48bd84d
daniel_rodriguez <daniel.rodriguez@sophossolutions.com>
2019-02-21 12:26:57 -0500

    Usando el Path del ChromeWeb Driver en el agente de devops

--- lib/pipelines/testapp.rb
+++ lib/pipelines/testapp.rb
@@ -5,6 +5,7 @@ require 'selenium-webdriver'
 class TestApp
 
     def ProbarGoogle
+        Selenium::WebDriver::Chrome.driver_path=ENV['ChromeWebDriver']
         browser=Selenium::WebDriver.for :chrome
         browser.navigate.to "http://www.google.com"
         #puts "Google Loaded in Chrome Browser"
